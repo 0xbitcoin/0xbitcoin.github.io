9eec3bc33d9beb66b3bca4e4d2d9bd6d8ff2a256
Andy <admazzola@gmail.com>
2018-02-02 07:48:54 -0500

    adding

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -4,6 +4,8 @@ import logo from '../images/0xbitcoin.png'
 import Vue from 'vue'
 
 
+var web3 = this.connectWeb3();
+
 
 var navbar = new Vue({
   el: '#navbar',
@@ -19,3 +21,22 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+
+connectWeb3(){
+  if (typeof web3 !== 'undefined') {
+
+        window.web3 = new Web3(web3.currentProvider);
+
+        return web3;
+
+  } else {
+
+
+      alertRenderer.renderError('No web3? You should consider trying MetaMask!')
+        return {}
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      //window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+  }
+}
