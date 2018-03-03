69f28ec0ca1074e511f0f067022f7cc0ea1e58f8
Andy <admazzola@gmail.com>
2018-03-03 16:48:25 -0500

    webpack

--- app/assets/pools/pool.list.js
+++ app/assets/pools/pool.list.js
@@ -1,7 +1,13 @@
 var list =  {pools:[
-   {name:"Token Mining Pool", url: "http://tokenminingpool.com" },
+   {name:"Token Mining Pool",
+   url: "http://tokenminingpool.com",
+  image: "/app/assets/img/0xpool.png"
+  },
 
-   {name:"0xPool" , url: "http://0xpool.io" }
+   {name:"0xPool" ,
+    url: "http://0xpool.io",
+     image: "/app/assets/img/0xpool.png"
+    }
 
  ]}
 
