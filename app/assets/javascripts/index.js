5d741ecf321ed9dea006fe33358d34de5de85859
Andy M <admazzola@gmail.com>
2018-03-05 10:49:34 -0500

    adding pool images

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,6 +5,12 @@ import logo from '../img/0xbitcoin.png'
 import githubLogo from '../img/GitHub-Mark-64px.png'
 import redditLogo from '../img/reddit-mark-64px.png'
 
+import brutePool from '../img/0xbrute.png'
+import TokenMiningPool from '../img/tokenminingpool.png'
+import _0xPool from '../img/0xpool.png'
+ 
+
+
 import Vue from 'vue'
 
 import AlertRenderer from './alert-renderer'
--- app/assets/pools/pool.list.js
+++ app/assets/pools/pool.list.js
@@ -1,12 +1,17 @@
 var list =  {pools:[
    {name:"Token Mining Pool",
    url: "http://tokenminingpool.com",
-  image: "/app/assets/img/0xpool.png"
+  image: "/app/assets/img/tokenminingpool.png"
   },
 
    {name:"0xPool" ,
     url: "http://0xpool.io",
      image: "/app/assets/img/0xpool.png"
+   },
+
+   {name:"0xBrute" ,
+    url: "http://0xbrute.com",
+     image: "/app/assets/img/0xbrute.png"
     }
 
  ]}
