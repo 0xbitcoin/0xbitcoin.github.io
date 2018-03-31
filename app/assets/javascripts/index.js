43e43e5ccbcb127a0f6f00d8f9d4aef52d4b4697
Andy <admazzola@gmail.com>
2018-03-31 00:48:03 -0400

    works better on mobile

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,6 +5,11 @@ import logo from '../img/0xbitcoin.png'
 import titleLogo from '../img/logo-dark.png'
 import githubLogo from '../img/GitHub-Mark-64px.png'
 import redditLogo from '../img/reddit-mark-64px.png'
+import contractQR from '../img/0xbitcoinContractQR.png'
+
+
+
+
 
 import brutePool from '../img/0xbtcpool.png'
 import TokenMiningPool from '../img/tokenminingpool.png'
@@ -35,7 +40,8 @@ var navbar = new Vue({
   data: {
     titleLogo: titleLogo,
     githubLogo: githubLogo,
-    redditLogo: redditLogo
+    redditLogo: redditLogo,
+    contractQR: contractQR
   }
 })
 
@@ -56,6 +62,76 @@ $(document).ready(function(){
 
       wallet.init(alertRenderer,ethHelper);
     }
+
+
+
+
+
+
+              // Get all "navbar-burger" elements
+              var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+              console.log($navbarBurgers.length)
+              // Check if there are any navbar burgers
+              if ($navbarBurgers.length > 0) {
+
+                // Add a click event on each of them
+                $navbarBurgers.forEach(function ($el) {
+                  $el.addEventListener('click', function () {
+
+                    // Get the target from the "data-target" attribute
+                    var target = $el.dataset.target;
+                    var $target = document.getElementById(target);
+
+                    // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                    $el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                  });
+                });
+              }
+
+
+
+              var $fadedin = $('.faded-in') ;
+                  console.log('meep');
+
+                if ($fadedin.length > 0) {
+                  $.each($fadedin,function () {
+                    var fadeTime = $(this).data('fade-time');
+                    console.log($(this));
+                    console.log(fadeTime);
+                   $(this).delay(fadeTime).animate({ opacity: 1 }, 400)
+                  });
+
+                }
+
+
+                var $activeDropdown = $('.dropdown-button') ;
+
+                if ($activeDropdown.length > 0) {
+                  $.each($activeDropdown,function () {
+                    $(this).on('click',function (){
+                        var parent = $(this).parent();
+                        var dropdownContent = $(parent).find('.navbar-dropdown').first();
+
+                        var visible = $(dropdownContent).css('display') == 'block';
+
+                          $('.navbar-dropdown').css('display','none');
+
+                        if(!visible)
+                        {
+                            $(dropdownContent).css('display','block');
+                            $(dropdownContent).hide();
+                            $(dropdownContent).fadeIn(100);
+                        }
+
+                    });
+
+
+                  });
+
+                }
 
 
 
