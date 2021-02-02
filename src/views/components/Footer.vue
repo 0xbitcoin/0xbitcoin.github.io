473651db8d2f84e9d04633e827ab03dc5ea24e6c
Andy <admazzola@gmail.com>
2021-02-02 16:36:23 -0500

    wiki link

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -4,7 +4,7 @@
       <div class="w-row">
         <div class="column-5 w-col w-col-4 w-col-small-4">
           <h3 class="heading-3 footer">Documentation</h3>
-         
+
           <router-link :to="{name: 'docs'}" class="footer-link">0xBitcoin API Wiki</router-link>
           <a href="https://github.com/0xbitcoin/white-paper" target="_blank" class="footer-link">White Paper</a>
           <a href="https://etherscan.io/address/0xb6ed7644c69416d67b522e20bc294a9a9b405b31#code" target="_blank" class="footer-link">Contract Code</a>
@@ -13,13 +13,16 @@
         <div class="column-6 w-col w-col-4 w-col-small-4">
           <h3 class="heading-3 footer">Community &amp; Social</h3>
           <a href="https://github.com/0xbitcoin" target="_blank" class="footer-link">0xBTC Github</a>
-          <a href="https://lavatoken.com/" target="_blank" class="footer-link">Lava Token</a>
           <a href="https://discordapp.com/invite/JGEqqmS" target="_blank" class="footer-link">Discord</a>
           <a href="https://twitter.com/0xbitcoin_erc20" target="_blank" class="footer-link">Twitter</a>
           <a href="https://www.reddit.com/r/0xbitcoin/" target="_blank" class="footer-link">Reddit</a></div>
         <div class="column-7 w-col w-col-4 w-col-small-4">
           <h3 class="heading-3 footer">Fun &amp; Learning</h3>
-          <a href="https://meoau.itch.io/zero-chan-learns-crypto" target="_blank" class="footer-link">Play Trav&#x27;s 0xBTC Game</a></div>
+          <a href="https://meoau.itch.io/zero-chan-learns-crypto" target="_blank" class="footer-link">Play Trav&#x27;s 0xBTC Game</a>
+            <a href="/#/docs" target="_blank" class="footer-link">0xBTC Wiki</a> 
+
+        </div>
+
       </div>
     </div>
   </div>
